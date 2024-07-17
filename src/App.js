7bc1a2f6cf6db58f89cff4f0b03cbb206bd8c184
hangzhao618 <zhaohang618@gmail.com>
2024-07-17 15:38:04 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import pic from "./illustration-article.svg";
+import authorImage from "./images/image-avatar.webp";
 
 function App() {
+  const category = "Learning";
+  const publishdate = "Published 21 Dec 2023";
+  const title = "HTML & CSS foundations";
+  const description =
+    "These languages are the backbone of every website, defining structure, content, and presentation.";
+  const authorName = "Greg Hooper";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div className="container">
+        <div className="blog-img">
+          <img src={pic} className="logo" alt="logo" />
+        </div>
+        <div className="content">
+          <div className="category">{category}</div>
+          <div className="publish-date sp">{publishdate}</div>
+          <div className="title sp">{title}</div>
+          <div className="description sp">{description}</div>
+        </div>
+        <div className="author">
+          <img className="author-img" src={authorImage} alt="author-img" />
+          <div className="author-name">{authorName}</div>
+        </div>
+      </div>
+      <div className="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
         </a>
-      </header>
+        . Coded by <a href="#">Hang</a>.
+      </div>
     </div>
   );
 }
